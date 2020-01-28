ceb5c9a28f1021445086b9e6b2377b150c863911
Nguyen D. Truong <awayakumo0192@gmail.com>
2020-01-28 18:55:42 +0900

    + Add data file

---
+++ Assets/Scripts/Data/LessonData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class LessonData : ScriptableObject
+{
+    [Serializable]
+    public class Lesson
+    {
+        public int id;
+        public int topic_id;
+        public string title;
+        public int order;
+        public string en, vi, zh, fr, de, hi, ind, it, ko, ms, pt, ru, es, th, tl, ar, pl, hr, nl, el, hu, sv, cs, ro, bg, da, tr, fi, sl, fa, bn, uk, am, km, si, zu, sw, lo, no;
+    }
+
+    public Lesson[] mListLessons;
+}
---
+++ Assets/Scripts/Data/TopicData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class TopicData : ScriptableObject
+{
+    [Serializable]
+    public class Topic
+    {
+        public int id;
+        public string title;
+        public int order;
+        public string en, vi, zh, fr, de, hi, ind, it, ko, ms, pt, ru, es, th, tl, ar, pl, hr, nl, el, hu, sv, cs, ro, bg, da, tr, fi, sl, fa, bn, uk, am, km, si, zu, no, sw, lo;
+    }
+
+    public Topic[] mListTopics;
+}
